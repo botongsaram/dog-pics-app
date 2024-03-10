675cf2743215f8e2a876f494d251a7aa4ac65625
Seungeun Lee <slee32268@gmail.com>
2024-03-09 22:42:10 -0800

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DogPics
+//
+//  Created by Seung Eun Lee on 3/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
